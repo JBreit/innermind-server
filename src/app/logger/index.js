edd28cd7b961c190ce2fddc770a150c735647a21
JBreit <jason.breitigan@gmail.com>
2017-05-15 00:34:25 -0400

    Added Index Route Test. TODO: Get Winston logger to work correctly.

--- src/app/logger/index.js
+++ src/app/logger/index.js
@@ -2,7 +2,7 @@ import winston from 'winston';
 
 winston.emitErrs = true;
 
-const logger = new winston.Logger({
+export const logger = new winston.Logger({
   transports: [
     new winston.transports.File({
       level: 'info',
@@ -23,6 +23,28 @@ const logger = new winston.Logger({
   exitOnError: false,
 });
 
-const stream = { write: (message, encoding) => { logger.info(message, encoding); } };
+/*export default class Logger extends winston.Logger {
+  constructor() {
+    super();
+    this.transports = [
+      new winston.transports.File({
+        level: 'info',
+        filename: './logs/all-logs.log',
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880,
+        maxFiles: 5,
+        colorize: false,
+      }),
+      new winston.transports.Console({
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+      }),
+    ];
+    this.exitOnError = false;
+  }
+}*/
 
-export default { logger, stream };
+export const stream = { write: (message, encoding) => { logger.info(message, encoding); } };
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import app from './app';
 const { HOST = '127.0.0.1' } = process.env;
 const { PORT = 8080 } = process.env;
 
-app.listen(PORT, HOST, () => { process.stdout.write(`Server running at http://${HOST}:${PORT}/`); });
+app.listen(PORT, HOST, () => { process.stdout.write(`Server running at http://${HOST}:${PORT}/\n`); });
